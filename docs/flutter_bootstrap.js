63136920ad9e96bbfdda5c4ef6e2c1c01c836135
waki.shibata <wawawawa922922922922@gmail.com>
2025-07-13 00:44:50 +0900

    Fix: Update base-href for GitHub Pages web build

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"72f2b18bb094f92f62a3113a8075240ebb59af
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3512403872"
+    serviceWorkerVersion: "3244071040"
   }
 });
