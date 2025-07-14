9e325718ebb5051830243ea48037cc84283fdd9f
waki.shibata <wawawawa922922922922@gmail.com>
2025-07-14 22:04:00 +0900

    feat: Deploy web app with walk deletion feature

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"72f2b18bb094f92f62a3113a8075240ebb59af
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2832669431"
+    serviceWorkerVersion: "1880717088"
   }
 });
